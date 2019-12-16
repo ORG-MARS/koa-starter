07782b817828db40a8659102ec886dcd7359bb0f
liangjunren <liangjunren@bytedance.com>
2019-12-16 17:09:33 +0800

    去掉环境变量

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-process.env.SERVICE_ID = 'ttboef6v7njs7s'
-process.env.SERVICE_TOKEN = '16add734-5122-4be9-a245-726ccd87517f'
-
 const app = require('./src/app')
 
 // 导出 http handler, koa 对象不可直接作为 http handler, 需要调用 callback() 获取
 module.exports = app.callback()
-
-// 当此文件作为入口文件时执行
-// 为本地调试提供
-if (!module.parent) {
-  app.listen(3001)
-  console.log('api server started: http://localhost:3001')
-}
